90244bb2dca4e70e5681e90b2203d8c22f9d17f4
CRUXsur <marcojtorrico@hotmail.com>
2022-05-08 19:27:48 -0400

    Geolocator - Android

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -17,5 +18,45 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
         isGpsPermissionGranted: event.isGpsPermissionGranted,
       )),
     );
+
+    //lo ponemos en el constructor
+    _init();
+  }
+
+  //me creo unos metodos que me permitan inicializar el servicio
+  //o inicializar el stream de informacion que va a venir
+  //proviniente del geolocator
+
+  //inicializacion:
+  Future<void> _init() async {
+    //
+    final isEnabled = await _checkGpsStatus();
+    print('isEnabled: $isEnabled');
+  }
+
+  //tenemos un listener que esta pendiente de cualquier cambio
+  //del servicio de GPS; pero es buena practica, que cuando se haga
+  //un listener, tenemos que limpiar ese listener, aunque este servicio
+  //o este bloc, nunca se va a cancelar, siempre esta pendiente de toda
+  //aplicacion siempre es bueno limpiarlo para evitar fugas de memoria
+  //
+  Future<bool> _checkGpsStatus() async {
+    //
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+
+    Geolocator.getServiceStatusStream().listen((event) {
+      //print('service status $event');
+      final isEnabled = (event.index == 1) ? true : false;
+      print('service status $isEnabled');
+    });
+    return isEnable;
+  }
+
+  //close se llama cuando el bloc ya no se va a utilizar o se va a
+  //deshechar
+  @override
+  Future<void> close() {
+    // TODO: limpiar el ServiceStatus stream
+    return super.close();
   }
 }
