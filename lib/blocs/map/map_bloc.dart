43cf6c1b1eb539cbd64e6a37da1653510dbc867f
CRUXsur <marcojtorrico@hotmail.com>
2022-05-19 20:41:44 -0400

    Obtener el controlador del mapa

--- lib/blocs/map/map_bloc.dart
+++ lib/blocs/map/map_bloc.dart
@@ -1,13 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart' show GoogleMapController, LatLng;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 part 'map_event.dart';
 part 'map_state.dart';
 
 class MapBloc extends Bloc<MapEvent, MapState> {
+  GoogleMapController? _mapController;
+
   MapBloc() : super(const MapState()) {
-    on<MapEvent>((event, emit) {
-      //
-    });
+    //on<OnMapInitializedEvent>((event, emit) => emit(state.copyWith(isMapInitialized: true)));
+    on<OnMapInitializedEvent>(_onInitMap);
+  }
+
+  void _onInitMap(OnMapInitializedEvent event, Emitter<MapState> emit) {
+    _mapController = event.controller;
+    //*_mapController. aqui tengo muchas cosas.....
+    //_mapController.
+    emit(state.copyWith(isMapInitialized: true));
   }
 }
